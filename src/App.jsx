955ffa791e6fbf7da5fff3cc5bfd6852a6a271c9
aamir-786 <aamir.fss22@gmail.com>
2025-04-14 15:54:44 +0500

    upload jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import ContactUs from './components/contactUS/contactus'
 import Login from './components/signin/Login'
 import Signup from './components/signup/Signup'
-
+import Upload from './components/Upload/Upload'
 const App = () => {
   return (
     <div>
       <Login/>
       <Signup/>
       <ContactUs/>
-
+      <Upload/>
     </div>
     
   )
